a5abe937b273d004dbbbbd82ae20ce26d5dffe30
CaritoTZ <95597613+CaritoTZ@users.noreply.github.com>
2021-12-06 17:01:20 -0300

    Update ahorcado_final.py

--- ahorcado_final.py
+++ ahorcado_final.py
@@ -4,6 +4,8 @@ import random
 import string
 from palabras_final import palabras
 
+#declaración de variables
+#matriz que va a contener la imagen cargada junto con su dimension
 imagenes = []
 idx_imagen = 0
 
@@ -45,7 +47,7 @@ def inicializar_imagenes():
     fondo_rect6 = fondo6.get_rect()
     imagenes.append([fondo6, fondo_rect6])
 
-    #funcion para el aleatorio de las palabras  
+#funcion para el aleatorio de las palabras del diccionario
 def obtener_palabra_valida(palabras):
     global palabra
 
@@ -57,8 +59,9 @@ def obtener_palabra_valida(palabras):
         palabra = random.choice(palabras)
     return palabra.upper()
     
-
+#funcion para actualizar palabra
 def actualizar_palabra():
+    #es el progreso de las letras adivinadas
     global texto 
     # Estado actual de la palabra que el jugador debe adivinar (por ejemplo:  H - L A)
     lista_palabra = [letra if letra in letras_adivinadas else '_' for letra in palabra]
@@ -75,28 +78,31 @@ def iniciar_juego ():
 
     idx_imagen = 0
 
+    #la variable palabra va a llamar a la funcion obtener_palabra_valida
+    #palabras hace referencia al diccionario importado
     palabra = obtener_palabra_valida(palabras)
-    letras_x_adivinar = set(palabra) #conjunto de letras de la palabra que deben ser adivinadas
-    abecedario = set(string.ascii_uppercase) #conjunto de letras en el abecedario
+    letras_x_adivinar = set(palabra) #conjunto de letras únicas de la palabra que deben ser adivinadas
+    abecedario = set(string.ascii_uppercase) #conjunto de letras en el abecedario sin ñ 
     letras_adivinadas = set() #letras que el usuario ha adivinado durante el juego 
     actualizar_palabra()
     vidas = 6
-
+#maneja la logica para determinar si la letra pasada x parametro pertenece a la palabra x adivinar
 def ahorcado(letra_usu):
     global vidas
-
+    #devuelve True si la letra es valida 
     resultado = False
-    #obtener respuesta del usuario mientras existen letras pendientes y al jugador le quedan vidas
+    #mientras existen letras pendientes y al jugador le quedan vidas
     if len(letras_x_adivinar) >0 and vidas >0:
         
         #Si la letra escogida por el usuario está en el abecedario
         # y no está en el conjunto de letras que ya se han ingresado,
-        # se añade la letra al conjunto de letras ingresadas.
+        # se añade la letra al conjunto de letras adivinadas.
         if letra_usu in abecedario - letras_adivinadas:
             letras_adivinadas.add(letra_usu)
             actualizar_palabra()
-        # Si la letra está en la palabra, quitar la letra 
-        # del conjunto de letras pendientes por adivinar.
+
+            # Si la letra está en la palabra, quitar la letra 
+            # del conjunto de letras pendientes por adivinar.
             if letra_usu in letras_x_adivinar:
                 letras_x_adivinar.remove(letra_usu)
                 resultado = True
@@ -107,6 +113,7 @@ def ahorcado(letra_usu):
         elif letra_usu in letras_adivinadas:
              resultado = True
     return resultado
+#funcion para validar que las letras ingresadas solo pertenezcan al alfabeto
 #El método isalpha() devuelve True si todos los caracteres 
 # son letras del alfabeto
 def es_valido (palabra):
@@ -117,8 +124,10 @@ def es_valido (palabra):
 
     return resultado
 
+#logica que arranca el juego, inicializa todos los modulos del pygame
 pygame.init()
 
+#declaracion de varibales para obtener los colores
 ROJO = (255, 0, 0)
 #GRIS = (123, 125, 125 )
 BLANCO = (255, 255, 255)
@@ -126,6 +135,7 @@ BLANCO = (255, 255, 255)
 NEGRO = (0,0,0)
 VERDE = (0,255,0)
 
+#dimensiones de la pantalla
 dimensiones = [710, 500]
 PANTALLA = pygame.display.set_mode(dimensiones)
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
@@ -135,10 +145,11 @@ pygame.display.set_caption("Ahorcado")
 icono = pygame.image.load("imagenes/ahorc.png")
 pygame.display.set_icon(icono)
 
-#myimage = pygame.load.image('my_image.jpg').convert()
+#llamo a las funciones de inicializar_imagenes e iniciar_juego
 inicializar_imagenes()
 iniciar_juego()
 
+#valida los eventos de la pantalla
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -146,11 +157,14 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if vidas >0 and len(letras_x_adivinar) >0:
+                #la letra presionada
                 letra = pygame.key.name(event.key).upper()
                 if es_valido(letra):
                     if not ahorcado(letra):
                         idx_imagen +=1
             else:
+                #si no tiene vidas (Perdio) o si adivino la palabra (gano) 
+                #al presionar cualquier tecla se inicia un nuevo juego
                 iniciar_juego()
     mensaje = ""
     color_mensaje = NEGRO
@@ -160,16 +174,21 @@ while True:
     if len(letras_x_adivinar) == 0:
         mensaje = "GANASTE!"
         color_mensaje = VERDE
+    # myfont.render: se usa para crear un texto que se va a dibujar en la pantalla.
+    #el primer parametro es el texto dibujar y el tercero es el color.
     texto_ahorcado = myfont.render(texto, False, (NEGRO))
     texto_vidas = myfont.render("Cantidad de vidas restantes: " + str(vidas), False, (NEGRO))
     texto_letras_ingresadas = myfont.render("Letras ingresadas:" + ' '.join(letras_adivinadas), False, (NEGRO))
+    #metodo blit para dibujar la imagen
     PANTALLA.fill(BLANCO)
+    #dibuja la imagen actual con su dimension, en el cero esta la imagen y en la posicion 1 su dimension
     PANTALLA.blit(imagenes[idx_imagen][0], imagenes[idx_imagen][1])
+    #dibuja el texto del ahorcado
     PANTALLA.blit(texto_ahorcado,(0,250))
     PANTALLA.blit(texto_vidas,(0,300))
     PANTALLA.blit(texto_letras_ingresadas,(0,350))
 
-    #pygame.display.flip()
+    #valida si el mensaje es != ""
     if mensaje != "":
         texto_mensaje = myfont.render(mensaje, False, (color_mensaje))
         PANTALLA.blit(texto_mensaje,(0,400))
